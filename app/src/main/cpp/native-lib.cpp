fdb11a23ed69d94634c88cbca0e4362cd332f46d
AndroidStorm <williamg.dev@gmail.com>
2018-02-13 11:18:04 -0800

    Run project

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,9 +2,11 @@
 #include <string>
 
 extern "C"
-JNIEXPORT jstring JNICALL
+JNIEXPORT jstring
+
+JNICALL
 Java_com_williamgdev_example_ndkproject_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
